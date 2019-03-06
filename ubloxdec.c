aaac846cda34185bc5478086e527bdfc23ba837a
AQU-YULU <32903229+AQU-YULU@users.noreply.github.com>
2019-03-06 10:09:17 +0800

    把imu数据means都处理了一下，然后在用matlab处理

--- ubloxdec.c
+++ ubloxdec.c
@@ -68,7 +68,7 @@ int main(int argc, char *argv[])
 			write_esf_raw_payload(payload, header->length);
 			ubx_free(payload);
 		}
-		*/
+		
 		if (is_ubx_nav_pvt(header)) {//UBX_NAV_PVT
 			write_nav_pvt_payload(payload, header->length);
 			ubx_free(payload);
